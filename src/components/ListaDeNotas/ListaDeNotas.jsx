8bf93ab71f85b69afacdbbeb7538b635d97d112c
João Paulo <joaopaulozangrande@gmail.com>
2021-03-16 16:34:47 -0300

    I organized the project

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,22 +1,21 @@
-import React from "react";
-import {Component} from "react";
-import CardNota from "../CardNota/CardNota";
+import React, {Component} from "react";
+import CardNota from "../CardNota";
 import "./estilo.css";
 
 class ListaDeNotas extends Component {
-
     render() {
         return (
-            <ul className="lista-notas">{Array.of("Trabalho","Trabalho","Estudos").map((categoria, index) => {
-            return (
-                <li className="lista-notas_item" key={index}>
-                    <CardNota/>
-                </li>
-            );
-        })}</ul>
+            <ul className="lista-notas">
+                {Array.of("Trabalho","Trabalho","Estudos").map((categoria, index) => {
+                    return (
+                        <li className="lista-notas_item" key={index}>
+                            <CardNota/>
+                        </li>
+                    );
+                })}
+            </ul>
         );
     }
-
 }
 
 export default ListaDeNotas;
